18a52778fa91f6df9d4bdbb6e0cd792433c9c5d5
Mattias Tengblad <EyesX@users.noreply.github.com>
2013-10-19 14:54:01 +0200

    X-UA-Compatible för IE

--- themes/wpsvse/header.php
+++ themes/wpsvse/header.php
@@ -9,6 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<meta http-equiv="X-UA-Compatible" content="IE=Edge" />
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
