ee5fd642391e10899e687eeee4192bdd01afd885
Mattias Tengblad <mst@eyesx.com>
2014-07-20 00:41:14 +0200

    Omformatering av footer enligt mall

--- themes/wpsvse/footer.php
+++ themes/wpsvse/footer.php
@@ -2,23 +2,36 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after
+ * Contains the footer with essential links.
  *
  * @package WordPress Sverige
  */
 ?>
 
-	</div><!-- #content -->
+	<!-- Start Footer -->
+	<footer id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5">
+					<p>Copyright &copy; 2007-<?php echo date('Y'); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="designer"><?php bloginfo( 'name' ); ?></a>
+				</div>
+				<div class="col-md-7">
+                  <!-- Start INSERT 2ND WP_NAV_MENU -->
+					<ul class="hidden-xs footer-links pull-right">
+						<li><a href="#">Cookies</a></li>
+						<li><a href="#">Användarvillkor/förhållningsregler</a></li>
+						<li><a href="#">Kontakt</a></li>
+					</ul>
+                  <!-- End INSERT 2ND WP_NAV_MENU -->
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Footer -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'wpsvse_credits' ); ?>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wpsvse' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'wpsvse' ), 'WordPress Sverige', '<a href="https://github.com/EyesX/wordpress-sverige-dev/" rel="designer">WordPress Sveriges Utvecklingsteam</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<!-- Back to Top -->	
+	<a href="#" class="back-to-top" title="Till toppen"><i class="fa fa-arrow-up"></i></a>
+	<!-- End Back to Top -->
 
 <?php wp_footer(); ?>
 
