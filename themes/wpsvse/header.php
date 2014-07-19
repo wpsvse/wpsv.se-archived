127a29be61382d54be4f93515bd921ea5c828cfc
Mattias Tengblad <mst@eyesx.com>
2014-07-20 00:32:22 +0200

    Omformatera header enligt skapad mall

--- themes/wpsvse/header.php
+++ themes/wpsvse/header.php
@@ -20,20 +20,30 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', 'wpsvse' ); ?></h1>
-			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'wpsvse' ); ?></a>
-
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+	<div class="top-part">
+		<!-- Start Header -->
+		<header id="header">
+			<!-- Start Navigation -->
+			<nav class="navbar navbar-inverse" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Start Logo / Text -->
+					<a class="navbar-brand text-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo-64.png" alt="logo"><span>WordPress</span>Sverige</a>
+					<!-- End Logo / Text -->
+				</div>
+				<div class="navbar-collapse collapse">
+					<!-- INSERT WP_NAV_MENU -->
+				</div>
+			</div>
+			</nav>
+		<!-- End Navigation -->
+		</header>
+		<!-- End Header -->
+	</div>
+	<!-- End Top-part -->
