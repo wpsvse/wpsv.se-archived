779aa050c2212db8d09727d3bd54a31abcdcfcaf
Mattias Tengblad <mst@eyesx.com>
2014-07-20 18:42:06 +0200

    Platshållare för blogg-tillägg

---
+++ plugins/wpsv-blog/wpsv_blog.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: WPSV Blogg (CPT+CT)
+Description: Anpassad posttyp och tillhörande taxonomi för WordPress Sveriges blogg.
+Version: 0.1
+License: GPL
+Author: WordPress Sverige
+Author URI: http://wpsv.se
+*/
+
+function wpsv_blog_init() {
+	// CPT+CT CODE GOES HERE DAMMIT!
+}
+add_action( 'init', 'wpsv_blog_init' );
+?>
