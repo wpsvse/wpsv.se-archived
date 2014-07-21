73305b3ae43a95d36165e9e18c5f6f055aad877b
Mattias Tengblad <mst@eyesx.com>
2014-07-22 01:04:08 +0200

    Ändringar för snabbknappar på startsidan

--- themes/wpsvse/front-page.php
+++ themes/wpsvse/front-page.php
@@ -73,36 +73,9 @@
     <section id="quick-buttons" class="section">
 		<div class="container">
 			<div class="row">
-              <!-- Start INSERT TEMPLATE PART FOR QUICK BUTTONS -->
-            	<div id="support-btn" class="col-md-3 quick-btn">
-                	<a href="#" class="quick-link">
-                        <i class="fa fa-medkit"></i>
-                        <h2>Support</h2>
-                        <span>Ta del av support för WordPress</span>
-                    </a>
-                </div>
-                <div id="forum-btn" class="col-md-3 quick-btn">
-                	<a href="#" class="quick-link">
-                        <i class="fa fa-comments"></i>
-                        <h2>Forum</h2>
-                        <span>Diskussioner och hjälp via vårt forum</span>
-                    </a>
-                </div>
-                <div id="files-btn" class="col-md-3 quick-btn">
-                	<a href="#" class="quick-link">
-                        <i class="fa fa-cloud-download"></i>
-                        <h2>Filer</h2>
-                        <span>Ladda ner översättningar, tillägg och teman</span>
-                    </a>
-                </div>
-                <div id="community-btn" class="col-md-3 quick-btn">
-                	<a href="#" class="quick-link">
-                        <i class="fa fa-group"></i>
-                        <h2>Community</h2>
-                        <span>Var en del av den svenska communityn</span>
-                    </a>
-                </div>
-              <!-- End INSERT TEMPLATE PART FOR QUICK BUTTONS -->
+              <!-- Start QUICK BUTTONS -->
+				<?php get_template_part( 'quickbuttons' ); ?>
+              <!-- End QUICK BUTTONS -->
             </div>
         </div>
     </section>
